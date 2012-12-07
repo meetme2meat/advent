e029ce37a73dc9e4ce27da03a0fa24035671804e
meetme2meat <meetme2meat@gmail.com>
2012-12-08 00:09:59 +0530

    Added The app correct

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,4 +1,4 @@
 puts "puts Creating Symbolic link"
 run "sed -i 's/mysql2/mysql/g' /data/#{app}/shared/config/database.yml"
-run "ln /data/#{appname}/shared/config/database.yml /data/#{app}/shared/config/keep.database.yml"
+run "ln /data/#{app}/shared/config/database.yml /data/#{app}/shared/config/keep.database.yml"
  

bce99ce96a8395708ee23679fd266653a69e65e9
meetme2meat <meetme2meat@gmail.com>
2012-12-08 01:13:28 +0530

    Modified

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,4 +1,4 @@
 puts "puts Creating Symbolic link"
-#run "sed -i 's/mysql2/mysql/g' /data/#{app}/shared/config/database.yml"
-run "ln /data/advent/shared/config/keep.database.yml /data/advent/shared/config/database.yml"
+run "sed -i 's/mysql2/mysql/g' /data/advent/shared/config/database.yml"
+run "cp  /data/advent/shared/config/database.yml /data/advent/shared/config/keep.database.yml"
  
